b78120e50223d80e5f7f6ca1ba5c8285f79feeec
jfleming3 <codingclub@css.edu>
2022-03-01 16:40:11 -0600

    python executing

--- src/css/comp/executionController.java
+++ src/css/comp/executionController.java
@@ -4,9 +4,23 @@
  */
 package css.comp;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 /**
  * FXML Controller class
@@ -15,6 +29,28 @@ import javafx.fxml.Initializable;
  */
 public class executionController implements Initializable {
 
+    @FXML
+    private TextArea userCode;
+    @FXML
+    private Button runButton;
+    @FXML
+    private Label label;
+    
+    
+    @FXML
+    private void runcode(ActionEvent event) throws MalformedURLException, ProtocolException, IOException {
+        
+        String code = userCode.getText();
+        
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+              new FileOutputStream("./DB/Testcode.py"), "utf-8"))) {
+        writer.write(code);
+}
+       
+        
+        
+    }
+    
     /**
      * Initializes the controller class.
      */
